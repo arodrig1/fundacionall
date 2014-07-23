ecc4fd72f22a2140a208f96b92eecf51279683c9
Alejandro Rodriguez <arodrig1@stanford.edu>
2014-07-23 00:23:39 -0700

    Fixed heroku start script

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ var errorHandlers = require('./config/error_handlers')(app);
 
 var routes = require('./routes')(app);
 
-module.exports = app;
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
